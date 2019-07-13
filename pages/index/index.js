935bd142cd6454248e75d1faa9fadef8200b87d7
Daisy881 <1069283643@qq.com>
2019-07-13 22:36:21 +0800

    基本完成tabbar的三个页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,124 @@
 const app = getApp()
 
 Page({
+  data: {
+    navbar: ['必玩', '攻略'],
+    currentTab: 0,
+    navFlag: '0',
+    classifyList: ['景点', '看海', '徒步登山', '游乐园撒欢', '最美乡村', '历史文化'],
+    currentCsf: 0,
+    classifyFlag: '0',
+    spotsList: [{
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }],
+    seaList: [{
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }],
+    mountainsList: [{
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }],
+    playList: [{
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }],
+    countryList: [{
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }],
+    historyList: [{
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }, {
+      detailsPic: '../../images/places/1.jpg',
+      detailsName: '深圳湾公园',
+      detailsPeople: '1987'
+    }],
+    strategysList: [{
+      strategysPic: '../../images/places/4.jpg',
+      strategysName: '吃过深圳的“大董”后，是种什么体验？',
+      strategysPeople: '1820'
+    }, {
+      strategysPic: '../../images/places/2.jpg',
+      strategysName: '深圳探店|四川麻辣火锅小龙坎（皇岗店）',
+      strategysPeople: '277'
+    }, {
+      strategysPic: '../../images/places/2.jpg',
+      strategysName: '吃过深圳的“大董”后，是种什么体验？',
+      strategysPeople: '1820'
+    }, {
+      strategysPic: '../../images/places/1.jpg',
+      strategysName: '吃过深圳的“大董”后，是种什么体验？',
+      strategysPeople: '1820'
+    }, {
+      strategysPic: '../../images/places/2.jpg',
+      strategysName: '吃过深圳的“大董”后，是种什么体验？',
+      strategysPeople: '1820'
+    }],
+    tribeList: [{
+      strategysPic: '../../images/places/3.jpg',
+      strategysName: '吃过深圳的“大董”后，是种什么体验？',
+      userHead: '',
+      username: '风萧萧',
+      strategysPeople: '43'
+    }, {
+      strategysPic: '../../images/places/1.jpg',
+      strategysName: '吃过深圳的“大董”后，是种什么体验？',
+      userHead: '',
+      username: '余公子',
+      strategysPeople: '231'
+    }, {
+      strategysPic: '../../images/places/2.jpg',
+      strategysName: '吃过深圳的“大董”后，是种什么体验？',
+      userHead: '',
+      username: '闲走深圳',
+      strategysPeople: '1234'
+    }]
+  },
   searchTo: function() {
     wx.navigateTo({
       url: '../../pages/logs/logs',
@@ -11,6 +129,20 @@ Page({
   choosePlace: function() {
     wx.navigateTo({
       url: '../../pages/logs/logs',
+    })
+  },
+  // 导航切换监听
+  navbarTap: function (e) {
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx,
+      navFlag: e.currentTarget.dataset.idx,
+    })
+  },
+  // 分类监听
+  classifyTap: function(e) {
+    this.setData({
+      currentCsf: e.currentTarget.dataset.idx,
+      classifyFlag: e.currentTarget.dataset.idx
     })
   }
 })
--- pages/square/square.js
+++ pages/square/square.js
@@ -2,6 +2,87 @@ const app = getApp()
 
 Page({
   data: {
-    msg: "广场..."
+    squareNav: ['推荐', '附近'],
+    currentTab: 0,
+    squareFlag: 0,
+    squareList: [{
+      userhead: '../../images/places/1.jpg',
+      username: '风萧萧啊',
+      dataTime: '2019-07-01',
+      articleContent: '航拍中国最美的丹霞地貌，到处五颜六色，仿佛童话世界',
+      picList: ['../../images/places/1.jpg', '../../images/places/1.jpg', '../../images/places/1.jpg'],
+      place: '南北公园',
+      praise: '23',
+      comment: '98',
+      share: ''
+    }, {
+      userhead: '../../images/places/1.jpg',
+      username: '风萧萧啊',
+      dataTime: '2019-07-01',
+      articleContent: '航拍中国最美的丹霞地貌，到处五颜六色，仿佛童话世界',
+      picList: ['../../images/places/1.jpg', '../../images/places/1.jpg', '../../images/places/1.jpg'],
+      place: '丹霞山',
+      praise: '23',
+      comment: '98',
+      share: ''
+    }],
+    nearbyList: [{
+      userhead: '../../images/places/2.jpg',
+      username: '风萧萧啊',
+      dataTime: '2019-07-01',
+      articleContent: '航拍中国最美的丹霞地貌，到处五颜六色，仿佛童话世界',
+      picList: ['../../images/places/2.jpg', '../../images/places/2.jpg', '../../images/places/2.jpg'],
+      distance: '12.7',
+      praise: '23',
+      comment: '98',
+      share: ''
+    }, {
+      userhead: '../../images/places/1.jpg',
+      username: '风萧萧啊',
+      dataTime: '2019-07-01',
+      articleContent: '航拍中国最美的丹霞地貌，到处五颜六色，仿佛童话世界',
+      picList: ['../../images/places/1.jpg', '../../images/places/1.jpg', '../../images/places/1.jpg'],
+      place: '丹霞山',
+      distance: '129.9',
+      praise: '23',
+      comment: '98',
+      share: ''
+    }, {
+      userhead: '../../images/places/1.jpg',
+      username: '风萧萧啊',
+      dataTime: '2019-07-01',
+      articleContent: '航拍中国最美的丹霞地貌，到处五颜六色，仿佛童话世界',
+      picList: ['../../images/places/1.jpg', '../../images/places/1.jpg', '../../images/places/1.jpg'],
+      place: '南北公园',
+      praise: '23',
+      comment: '98',
+      share: ''
+    }, {
+      userhead: '../../images/places/1.jpg',
+      username: '风萧萧啊',
+      dataTime: '2019-07-01',
+      articleContent: '航拍中国最美的丹霞地貌，到处五颜六色，仿佛童话世界',
+      picList: ['../../images/places/1.jpg', '../../images/places/1.jpg', '../../images/places/1.jpg'],
+      distance: '2.8',
+      praise: '23',
+      comment: '98',
+      share: ''
+    }]
+  },
+  // 监听导航
+  squareTap: function(e) {
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx,
+      squareFlag: e.currentTarget.dataset.idx
+    })
+  },
+  praiseTap: function() {
+    console.log('喜欢...')
+  },
+  commentTap: function() {
+    console.log('评论...')
+  },
+  shareTap: function() {
+    console.log("分享...")
   }
 })
